41d3d6c4519ebebe0e8a67a944f394154c5492a7
Danial-dene <denefar5@gmail.com>
2023-04-13 15:26:23 +0800

    add meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <link className="rounded-lg " rel="icon" href="/assets/logo.png" /> */}
+
+          <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
           <link
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
